89b232edf05af7ce880ebe1f474575da92c4d4f9
Bagas Saras <sarasbagas63@gmail.com>
2024-04-26 16:23:19 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Stringable;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$12$84P4qkXesTbrejlABkgKmuWMfBOiA9ZwIjkAfvGSKDhgIzkqBE5tS',
+                'remember_token' => Str::random(10),
+                'is_admin' => true
+            ]
+        );
+        User::factory(100)->create();
+        Todo::factory(500)->create();
     }
 }
